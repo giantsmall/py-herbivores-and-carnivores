5a4c4ec55a236ee1e1503ca55ce3b783ef6b4f96
Marek Smolana <msmolana@gmail.com>
2025-02-03 12:08:55 +0100

    fixes to solution

--- app/main.py
+++ app/main.py
@@ -1,14 +1,9 @@
 class Animal:
     alive = []
 
-    def __str__(self) -> str:
-        return {"Name": self.name,
-                "Health": self.health,
-                "Hidden": self.hidden}
-
-    def __init__(self, name: str, health: int = 100) -> None:
+    def __init__(self, name: str, health: int = 100, hidden: bool = False) -> None:
         self.name = name
-        self.hidden = False
+        self.hidden = hidden
         self.health = health
         Animal.alive.append(self)
         print(f"{self.name}, total animals: {len(Animal.alive)}")
@@ -16,9 +11,8 @@ class Animal:
     def getbitten(self) -> None:
         pass
 
-    def __iter__(self) -> str:
-        return self.__str__
-
+    def __repr__(self) -> str:
+        return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
 
 class Carnivore(Animal):
     def bite(self, animal: Animal) -> None:
